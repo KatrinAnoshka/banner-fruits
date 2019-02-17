e9636f3aab6324a585782e66077da6918defe8f8
KatrinAnoshka <katrinanoshka@gmail.com>
2019-02-17 21:58:48 +0300

    added blur and duration vars

--- script.js
+++ script.js
@@ -30,24 +30,25 @@
 
         function animate() {      
 
+        var duration = 4.5;
         tl
             .addLabel("fr1")
 
-            .to(rasberry, 3.5,{rotation:10, y:"-=15", x:"-=20", ease: Power1.easeInOut}, "fr1")
-            .to(lime, 3.5,{rotation:-10, y:"-=25", x:"+=20", ease: Power1.easeInOut}, "fr1")
-            .to(mandarin, 3.5,{rotation:12, y:"+=15", x:"+=70", ease: Power1.easeInOut}, "fr1")
-            .to(round1, 3.5,{rotation:20, y:"+=30", x:"-=25", ease: Power1.easeInOut}, "fr1")
-            .to(round2, 3.5,{rotation:-15, y:"-=15", x:"+=20", ease: Power1.easeInOut}, "fr1")
-            .to(round3, 3.5,{rotation:35, y:"-=35", x:"+=25", ease: Power1.easeInOut}, "fr1")
+            .to(rasberry, duration,{rotation:-5, y:"-=38", x:"-=65", ease: Power1.easeInOut}, "fr1")
+            .to(lime, duration,{rotation:-10, y:"-=30", x:"+=48", ease: Power1.easeInOut}, "fr1")
+            .to(mandarin, duration,{rotation:12, y:"+=35", x:"+=90", ease: Power1.easeInOut}, "fr1")
+            .to(round1, duration,{rotation:20, 'webkitFilter': 'blur(0)', y:"+=30", x:"-=75", ease: Power1.easeInOut}, "fr1")
+            .to(round2, duration,{rotation:-15, 'webkitFilter': 'blur(0)', y:"-=25", x:"-=80", ease: Power1.easeInOut}, "fr1")
+            .to(round3, duration,{rotation:35, 'webkitFilter': 'blur(0)', y:"-=35", x:"+=55", ease: Power1.easeInOut}, "fr1")
 
-            .addLabel("fr2", "3.5")
+            .addLabel("fr2", "4.5")
 
-            .to(rasberry, 3.5,{rotation:-10, y:"+=15", x:"0", ease: Power1.easeInOut}, "fr2")
-            .to(lime, 3.5,{rotation:-10, y:"+=15", x:"0", ease: Power1.easeInOut}, "fr2")
-            .to(mandarin, 3.5,{rotation:-10, y:"+=15", x:"0", ease: Power1.easeInOut}, "fr2")
-            .to(round1, 3.5,{rotation:-10, 'webkitFilter': 'blur(0.6px)', y:"+=15", x:"0", ease: Power1.easeInOut}, "fr2")
-            .to(round2, 3.5,{rotation:-10, 'webkitFilter': 'blur(1px)', y:"+=15", x:"0", ease: Power1.easeInOut}, "fr2")
-            .to(round3, 3.5,{rotation:-10, 'webkitFilter': 'blur(0.2px)', y:"+=15", x:"40", ease: Power1.easeInOut}, "fr2")
+            .to(rasberry, duration,{rotation:10, zIndex:3, y:"+=15", x:"0", ease: Power1.easeInOut}, "fr2")
+            .to(lime, duration,{rotation:-10, zIndex:6, y:"+=15", x:"-=122", ease: Power1.easeInOut}, "fr2")
+            .to(mandarin, duration,{rotation:-10, zIndex:1, y:"-=15", x:"-=42", ease: Power1.easeInOut}, "fr2")
+            .to(round1, duration,{rotation:-10, 'webkitFilter': 'blur(0.6px)', y:"+=15", x:"0", ease: Power1.easeInOut}, "fr2")
+            .to(round2, duration,{rotation:-10, 'webkitFilter': 'blur(0.6px)', y:"-=5", x:"+=185", ease: Power1.easeInOut}, "fr2")
+            .to(round3, duration,{rotation:-10, 'webkitFilter': 'blur(0.6px)', y:"+=15", x:"40", ease: Power1.easeInOut}, "fr2")
 
         }  
 
